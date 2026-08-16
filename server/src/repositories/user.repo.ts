8792f880e7f7aea8a29eef0da209fc670d40b3c1
Mostafa gamal <44785872+mostafa2425@users.noreply.github.com>
2026-08-17 00:15:16 +0300

    refactor(server): update Prisma import

--- server/src/repositories/user.repo.ts
+++ server/src/repositories/user.repo.ts
@@ -1,5 +1,5 @@
 import { prisma } from '../utils/prisma';
-import { Prisma } from '@prisma/client';
+import { Prisma } from '../prisma';
 
 export class UserRepository {
   async findById(id: string) {
