7495d5ca2e8ff9a8b6f2639ea646b48f6d93d8de
Mostafa gamal <44785872+mostafa2425@users.noreply.github.com>
2026-08-18 15:42:07 +0300

    feat: expose operation follow-up APIs

--- server/src/routes/operation.routes.ts
+++ server/src/routes/operation.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { operationController } from '../controllers/operation.controller';
+import { operationFollowUpController } from '../controllers/operationFollowUp.controller';
 import { authMiddleware } from '../middlewares/auth.middleware';
 import { uploadOperationFiles } from '../middlewares/upload';
 
@@ -8,6 +9,10 @@ const router = Router();
 router.get('/', authMiddleware, operationController.getAll.bind(operationController));
 router.get('/:id', authMiddleware, operationController.getById.bind(operationController));
 router.get('/:id/timeline', authMiddleware, operationController.getTimeline.bind(operationController));
+router.get('/:id/follow-ups', authMiddleware, operationFollowUpController.list.bind(operationFollowUpController));
+router.post('/:id/follow-ups', authMiddleware, operationFollowUpController.create.bind(operationFollowUpController));
+router.patch('/:id/follow-ups/:followUpId', authMiddleware, operationFollowUpController.update.bind(operationFollowUpController));
+router.delete('/:id/follow-ups/:followUpId', authMiddleware, operationFollowUpController.remove.bind(operationFollowUpController));
 router.get('/:operationId/files/:fileId/download', authMiddleware, operationController.downloadFile.bind(operationController));
 router.post('/', authMiddleware, operationController.create.bind(operationController));
 router.put('/:id', authMiddleware, operationController.update.bind(operationController));
